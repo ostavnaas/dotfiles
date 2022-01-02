ce750fa50ca1fe26c42170aeff062ba2acdac874
Ove Stavnås <22543460+ostavnaas@users.noreply.github.com>
2022-01-02 15:52:47 +0100

    Fix virsh-ssh

--- scripts/virsh-ssh.sh
+++ scripts/virsh-ssh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh $(virsh domifaddr $1 --source agent| grep ipv4 |grep -v lo| awk '{ print $4 }' | cut -d / -f 1)
+ssh $(virsh domifaddr $1 --source agent --interface enp1s0 | grep ipv4 |grep -v lo| awk '{ print $4 }' | cut -d / -f 1)
