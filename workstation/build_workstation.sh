5b7c3a977212a6d9c45d933f655c963230ceb9b9
Ove Stavnås <o.stavnas@sportradar.com>
2022-06-22 12:45:16 +0200

    Updates

--- workstation/build_workstation.sh
+++ workstation/build_workstation.sh
@@ -3,7 +3,13 @@
 set -eux
 
 
-sudo apt-get install keepassx vim git tmux zsh curl
+sudo apt-get update
+sudo apt-get install keepassx vim git tmux zsh curl \
+                     iputils-arping net-tools tcptraceroute \
+                     python3-venv python3-pip jq cmake ethtool \
+                     xclip sipcalc fonts-powerline iptables-persistent \
+                     whois dnsutils -y
+
 
 
 
@@ -14,15 +20,15 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # https://github.com/VundleVim/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-mkdir ~/github/ostavnaas
-git clone git@github.com:ostavnaas/dotfiles.git
+mkdir -p ~/github/ostavnaas
+git clone git@github.com:ostavnaas/dotfiles.git ~/github/ostavnaas/dotfiles/
 ln -fs ~/github/ostavnaas/dotfiles/.zshrc ~/.zshrc
 ln -fs ~/github/ostavnaas/dotfiles/.vimrc ~/.vimrc
 ln -fs ~/github/ostavnaas/dotfiles/.tmux.conf ~/.tmux.conf
 
 
 # Gnome terminal
-dconf load /org/gnome/terminal/legacy/profiles:/ < $(dirname "$0")/files/gnome-terminal
+dconf load /org/gnome/terminal/legacy/profiles:/ < ~/github.com/ostavnaas/dotfiles/workstation/files/gnome-terminal
 
 
 # Disable non-breaking space
@@ -45,3 +51,6 @@ echo "set-default-source alsa_input.usb-046d_HD_Pro_Webcam_C920_F3E259AF-02.anal
 sudo sed -E 's/#HandleLidSwitch=(\w+)/HandleLidSwitch=poweroff/' -i /etc/systemd/logind.conf
 sudo systemctl restart systemd-logind.service
 
+
+sudo pip3 install httpcode
+sudo chsh $(whoami) --shell=/bin/zsh
