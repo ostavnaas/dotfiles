8af48b7fa122911b0d7aa2f3f1f7319c47050389
Ove Stavnås <22543460+ostavnaas@users.noreply.github.com>
2024-05-13 08:47:03 +0200

    Linux exit codes in errno.h

--- debug/ffmpeg_error.c
+++ debug/ffmpeg_error.c
@@ -1,4 +1,3 @@
-
 #define MKTAG(a,b,c,d)   ((a) | ((b) << 8) | ((c) << 16) | ((unsigned)(d) << 24))
 #define FFERRTAG(a, b, c, d) (-(int)MKTAG(a, b, c, d))
 
---
+++ debug/linux_exit_code.c
@@ -0,0 +1,7 @@
+#include <errno.h>
+// https://man7.org/linux/man-pages/man3/errno.3.html
+// https://en.wikipedia.org/wiki/Errno.h
+int main() {
+
+return ENOENT;
+}
