752b130d7a0ad74ec6c710c9ec7385ac1bc03c73
Ove Stavnås <22543460+ostavnaas@users.noreply.github.com>
2024-10-22 14:16:05 +0200

    Bunches

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,5 @@ local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 vim.o.timeoutlen = 1000
+vim.api.nvim_set_keymap('n', '<leader>dd', ':%bd|e#<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>da', ':%bd|e#\'<CR>', { noremap = true, silent = true })
