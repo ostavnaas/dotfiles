b0801c87eae6557df3cd8acd168fc8166b22625d
Ove Stavnås <22543460+ostavnaas@users.noreply.github.com>
2026-06-08 08:00:36 +0200

    Fix zero-day-mitigation

--- scripts/zero-day-mitigation.sh
+++ scripts/zero-day-mitigation.sh
@@ -1,9 +1,9 @@
-cat << EOF > etc/sysctl.d/ptrace-restrict.conf
+sudo tee /etc/sysctl.d/ptrace-restrict.conf > /dev/null <<'EOF'
 # CVE-2026-46333
 kernel.yama.ptrace_scope=2
 EOF
 
-cat << EOF > /etc/modprobe.d/disable-algif_aead.conf
+sudo tee /etc/modprobe.d/disable-algif_aead.conf > /dev/null <<'EOF'
 # Disable algif_aead module due to CVE-2026-31431 (AKA copy.fail)
 # This will likely be re-enabled in a subsequent update once an updated
 # kernel has been deployed.
@@ -12,7 +12,7 @@ install algif_aead /bin/false
 EOF
 
 
-cat << EOF /etc/modprobe.d/dirtyfrag.conf
+sudo tee /etc/modprobe.d/dirtyfrag.conf > /dev/null <<'EOF'
 install esp4 /bin/false
 install esp6 /bin/false
 install rxrpc /bin/false
